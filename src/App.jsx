93520db4c8dbfe015b8f724283f8e421ab73474e
Christian Fernandez <christian14@gmail.com>
2023-12-05 10:51:44 -0300

    Trabajando con el Formulario / Estructura basica y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Formulario from "./components/Formulario";
 import { Container, Grid, Typography } from "@mui/material";
 function App() {
 
@@ -6,14 +6,31 @@ function App() {
     <Container>
       <header>
         <Typography
-        align="center" 
-        marginY={5} 
-        component="h1"
-        variant="h3"
+          align="center"
+          marginY={5}
+          component="h1"
+          variant="h3"
         >
           Buscador de Noticias
         </Typography>
       </header>
+
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid
+          item
+          md={6}
+          xs={12}
+          lg={4}
+
+        >
+          <Formulario />
+        </Grid>
+      </Grid>
     </Container>
   )
 }
